6cd3144a3755948f4106f331ea1db8a80ce28c99
mohamilr <ibrahimdamy7@gmail.com>
2023-06-11 23:35:40 +0100

    ch: update form schema

--- src/validations/form.validation.js
+++ src/validations/form.validation.js
@@ -1,7 +1,11 @@
 import { object, string } from "yup";
 
 export const formSchema = object({
-  fullName: string().min(3).required("please enter you full name"),
+  fullName: string()
+    .matches(/^[a-zA-Z ]*$/, "must be a string")
+    .matches(/^\s*[\S]+(\s[\S]+)+\s*$/gms, "Please enter two names")
+    .min(3)
+    .required("please enter your full name"),
   email: string().email().required(),
   password: string().min(8).max(15).required(),
   url: string().url().required(),
